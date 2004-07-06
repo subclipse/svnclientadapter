5b69111c0a264654775a9fef5c455a8eafc91422
McClain Looney <mml@tigris.org>
2004-07-06 21:40:51 +0000

    added constants interface for people who like _svn

---
+++ src/main/org/tigris/subversion/svnclientadapter/SVNConstants.java
@@ -0,0 +1,10 @@
+package org.tigris.subversion.svnclientadapter;
+
+public interface SVNConstants {
+    public static final String SVN_DIRNAME = ".svn"; 
+    public static final String SVN_ENTRIES = "entries";
+    public static final String SVN_DIRPROPS = "dir-props";
+    public static final String SVN_PROPS = "props";
+
+
+}
