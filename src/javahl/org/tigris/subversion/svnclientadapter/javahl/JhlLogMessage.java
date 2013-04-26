fbe87d2fcad6f91854850f23d69ac2a7a97266c2
Steve Elsemore <azulfool@gmail.com>
2013-04-26 18:40:41 +0000

    Catch all extensions when creating LogDate from JhlLogMessage constructor so that no unexpected JavaHL exception causes a problem.

--- src/javahl/org/tigris/subversion/svnclientadapter/javahl/JhlLogMessage.java
+++ src/javahl/org/tigris/subversion/svnclientadapter/javahl/JhlLogMessage.java
@@ -64,7 +64,7 @@ public class JhlLogMessage implements ISVNLogMessage {
 		this.hasChildren = hasChildren;
 		try {
 			logDate = new LogDate(new String(this.revprops.get(DATE)));
-		} catch (ParseException e) {
+		} catch (Exception e) {
 		}
 	}
 
