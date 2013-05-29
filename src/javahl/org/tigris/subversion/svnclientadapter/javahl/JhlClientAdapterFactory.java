61058e6d9c842dc5b85a51101d725ac2fb34f4f3
Mark Phippard <markphip@gmail.com>
2013-05-29 18:43:24 +0000

    Update to SVN 1.8-rc2

--- src/javahl/org/tigris/subversion/svnclientadapter/javahl/JhlClientAdapterFactory.java
+++ src/javahl/org/tigris/subversion/svnclientadapter/javahl/JhlClientAdapterFactory.java
@@ -182,15 +182,15 @@ public class JhlClientAdapterFactory extends SVNClientAdapterFactory {
     			try {
 	                ISVNClient svnClient = new SVNClient();
     				Version version = svnClient.getVersion();
-    				if (version.getMajor() == 1 && version.getMinor() == 7)
+    				if (version.getMajor() == 1 && version.getMinor() == 8)
     					available = true;
     				else {
     					available = false;
-    					javaHLErrors = new StringBuffer("Incompatible JavaHL library loaded.  Subversion 1.7.x required.");
+    					javaHLErrors = new StringBuffer("Incompatible JavaHL library loaded.  Subversion 1.8.x required.");
     				}
     			} catch (UnsatisfiedLinkError e) {
     				available = false;
-    				javaHLErrors = new StringBuffer("Incompatible JavaHL library loaded.  1.7.x or later required.");
+    				javaHLErrors = new StringBuffer("Incompatible JavaHL library loaded.  1.8.x or later required.");
     			}
     		}
     	}
