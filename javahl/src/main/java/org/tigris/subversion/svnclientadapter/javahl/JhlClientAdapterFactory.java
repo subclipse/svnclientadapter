cb669d63e9c44b7407b19f613def595a6061e7da
Mark Phippard <mphippard@collab.net>
2020-04-27 15:51:23 -0400

    Update to SVN 1.13.0

--- javahl/src/main/java/org/tigris/subversion/svnclientadapter/javahl/JhlClientAdapterFactory.java
+++ javahl/src/main/java/org/tigris/subversion/svnclientadapter/javahl/JhlClientAdapterFactory.java
@@ -183,15 +183,15 @@ public class JhlClientAdapterFactory extends SVNClientAdapterFactory {
     			try {
 	                ISVNClient svnClient = new SVNClient();
     				Version version = svnClient.getVersion();
-    				if (version.getMajor() == 1 && version.getMinor() == 12)
+    				if (version.getMajor() == 1 && version.getMinor() == 13)
     					available = true;
     				else {
     					available = false;
-    					javaHLErrors = new StringBuffer("Incompatible JavaHL library loaded.  Subversion 1.12.x required.");
+    					javaHLErrors = new StringBuffer("Incompatible JavaHL library loaded.  Subversion 1.13.x required.");
     				}
     			} catch (UnsatisfiedLinkError e) {
     				available = false;
-    				javaHLErrors = new StringBuffer("Incompatible JavaHL library loaded.  1.12.x or later required.");
+    				javaHLErrors = new StringBuffer("Incompatible JavaHL library loaded.  1.13.x required.");
     			}
     		}
     	}
